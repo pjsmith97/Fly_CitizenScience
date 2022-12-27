48a2ae5c6f70b437375fa4575837e8086295e1b7
pjsmith97 <pjsmith997@yahoo.com>
2022-12-27 05:21:14 +0000

    More Comments

--- Assets/Scripts/MainMenu/MenuStates/MenuState.cs
+++ Assets/Scripts/MainMenu/MenuStates/MenuState.cs
@@ -2,17 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-/***************************************************************************************
-*    Title: TurretState
-*    Author: inScope Studios
-*    Date: Mar 17, 2021
-*    Edit: Aug 29, 2022
-*    Edit Author: Philip Smith
-*    Code version: 1.0
-*    Availability: https://youtu.be/VnfD5wGEXFw
-*
-***************************************************************************************/
-
 public abstract class MenuState
 {
     protected MenuController menu;
--- Assets/Scripts/UIAnimationStates/MainMenuUI/MainMenuTutorialUIState.cs
+++ Assets/Scripts/UIAnimationStates/MainMenuUI/MainMenuTutorialUIState.cs
@@ -2,6 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/***************************************************************************************
+*    Title: MainMenuTutorialState
+*    Original Title: TurretState
+*    Author: inScope Studios
+*    Date: Mar 17, 2021
+*    Edit: Aug 29, 2022
+*    Edit Author: Philip Smith
+*    Code version: 1.0
+*    Availability: https://youtu.be/VnfD5wGEXFw
+*    Description: Abstract finite state machine class for Main Menu Tutorial prompt UI
+*
+***************************************************************************************/
+
 public abstract class MainMenuTutorialUIState
 {
     protected MenuTutorialStateHelper menuHelper;
